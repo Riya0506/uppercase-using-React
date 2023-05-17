da9d125ac4e2933fcc42d748bbd721518ded3c1f
Riya0506 <67059248+Riya0506@users.noreply.github.com>
2023-05-17 22:02:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+
+function App() {
+  return (
+  <>
+   
+<Navbar title="Textutils"/>
+<div className="container my-3">
+<TextForm heading="Enter text to analyze"/>
+</div>
+
+</>
+
+  );
+}
+
+export default App;
